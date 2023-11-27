78bd8c1dda7deb61d693a3d0302472ca1dbe360f
Daniel Henrique <113483288+Henriqueriano@users.noreply.github.com>
2023-11-27 12:03:51 -0300

    Update Armazem.java

--- Armazem.java
+++ Armazem.java
@@ -1,3 +1,6 @@
+// Created by Daniel Henrique
+// File with the champion copied from https://ge.globo.com/esports/lol/noticia/2022/02/18/campeoes-do-lol-lista-completa-de-personagens-e-funcoes.ghtml 
+// The file is edit
 import java.io.File;
 import java.util.ArrayList;
 abstract class Armazem
